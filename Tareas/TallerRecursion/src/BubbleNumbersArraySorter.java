228dba1059ad95abe1b9e78080b5449f600a66b8
Omar Toledo <o.toledo@uniandes.edu.co>
2024-08-23 20:49:28 -0500

    Bubble Sort - Homeowrk 2

--- Tareas/TallerRecursion/src/BubbleNumbersArraySorter.java
+++ Tareas/TallerRecursion/src/BubbleNumbersArraySorter.java
@@ -4,11 +4,24 @@ import java.util.List;
  * Implements the bubble sort algorithm for number arrays
  */
 public class BubbleNumbersArraySorter implements NumbersArraySorter {
-
+	/**
+	 * Sorts the given numbers in ascending order using bubble sort algorithm
+	 * @param numbers to sort
+	 */
 	@Override
 	public void sort(List<Integer> numbers) {
 		// TODO Implement
-		
+		int n = numbers.size()-1;
+		int swap;
+		for(int i=0;i<=n-1;i++) {
+			for(int j=n; j>=i+1; j--) {
+				if (numbers.get(j)<numbers.get(j-1)) {
+					swap = numbers.get(j);
+					numbers.set(j,numbers.get(j-1));
+					numbers.set(j-1,swap);
+				}
+			}
+		}
 	}
 
 }
