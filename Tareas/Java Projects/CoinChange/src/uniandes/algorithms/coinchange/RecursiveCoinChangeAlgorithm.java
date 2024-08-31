b12e87996766b8d8b5642e000b9ab0420be6459c
omardtl24 <o.toledo@uniandes.edu.co>
2024-08-31 15:06:17 -0500

    Reorganize Folder Structure

---
+++ Tareas/Java Projects/CoinChange/src/uniandes/algorithms/coinchange/RecursiveCoinChangeAlgorithm.java
@@ -0,0 +1,14 @@
+package uniandes.algorithms.coinchange;
+
+public class RecursiveCoinChangeAlgorithm implements CoinChangeAlgorithm{
+    /**
+	 * Returns the solution of the CoinChange problem by a recursive approach
+	 * @param totalValue to reach with the coins
+     * @param denominations of the coins to choose
+	 */
+	@Override
+	public int [] calculateOptimalChange(int totalValue, int [] denominations){
+        int [] answer = new int[denominations.length];
+        return answer;
+    }
+}
